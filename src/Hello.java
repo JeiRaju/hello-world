600726e1eb31577a70def9ca3750afd08c1b4699
JeiRaju <48069959+JeiRaju@users.noreply.github.com>
2019-09-13 15:52:58 +0530

    file upload
    added source files

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Jei!");
+
+        int myFirstNumber = (5 + 10) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myBalance = 1000 - myTotal;
+
+        System.out.println(myTotal);
+        System.out.println(myBalance);
+    }
+}
